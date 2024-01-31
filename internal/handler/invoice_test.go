5151eac25f07697cc0547579977853321fb06792
ahrojoml <andres.huerta@mercadolibre.cl>
2024-01-31 09:22:46 -0300

    tests(invoices): add tests for update invoices

--- internal/handler/invoice_test.go
+++ internal/handler/invoice_test.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/DATA-DOG/go-txdb"
@@ -162,5 +163,208 @@ func TestInvoicesTotalByCondition(t *testing.T) {
 			require.Equal(t, testCase.expectCode, response.Code)
 			require.JSONEq(t, testCase.expectBody, response.Body.String())
 		})
+	}
+}
+
+func TestUpdateInvoicesTotal(t *testing.T) {
+	testCases := []struct {
+		name         string
+		products     []internal.Product
+		invoices     []internal.Invoice
+		sales        []internal.Sale
+		expectCode   int
+		expectValues map[int]float64
+	}{
+		{
+			name: "success retrieve invoices total by condition",
+			products: []internal.Product{
+				{
+					Id: 1,
+					ProductAttributes: internal.ProductAttributes{
+						Description: "Product 1",
+						Price:       10.00,
+					},
+				},
+				{
+					Id: 2,
+					ProductAttributes: internal.ProductAttributes{
+						Description: "Product 2",
+						Price:       5.00,
+					},
+				},
+			},
+			invoices: []internal.Invoice{
+				{
+					Id: 1,
+					InvoiceAttributes: internal.InvoiceAttributes{
+						Datetime:   "2022-05-15 00:00:00",
+						Total:      0.00,
+						CustomerId: 1,
+					},
+				}, {
+					Id: 2,
+					InvoiceAttributes: internal.InvoiceAttributes{
+						Datetime:   "2022-05-15 00:00:00",
+						Total:      0.00,
+						CustomerId: 1,
+					},
+				},
+			},
+			sales: []internal.Sale{
+				{
+					Id: 1,
+					SaleAttributes: internal.SaleAttributes{
+						ProductId: 1,
+						InvoiceId: 1,
+						Quantity:  10,
+					},
+				},
+				{
+					Id: 2,
+					SaleAttributes: internal.SaleAttributes{
+						ProductId: 2,
+						InvoiceId: 1,
+						Quantity:  10,
+					},
+				},
+				{
+					Id: 3,
+					SaleAttributes: internal.SaleAttributes{
+						ProductId: 1,
+						InvoiceId: 2,
+						Quantity:  20,
+					},
+				},
+			},
+			expectCode: http.StatusOK,
+			expectValues: map[int]float64{
+				1: 150.00,
+				2: 200.00,
+			},
+		},
+	}
+
+	for idx, testCase := range testCases {
+		t.Run(fmt.Sprintf("%d - %s", idx, testCase.name), func(t *testing.T) {
+			db, err := sql.Open("txdb", "fantasy_products_test")
+			require.NoError(t, err)
+			defer db.Close()
+
+			defer func(db *sql.DB) {
+				// delete records
+				_, err := db.Exec("DELETE FROM sales")
+				if err != nil {
+					panic(err)
+				}
+				_, err = db.Exec("DELETE FROM products")
+				if err != nil {
+					panic(err)
+				}
+				_, err = db.Exec("DELETE FROM invoices")
+				if err != nil {
+					panic(err)
+				}
+				_, err = db.Exec("DELETE FROM customers")
+				if err != nil {
+					panic(err)
+				}
+				// reset auto increment
+				_, err = db.Exec("ALTER TABLE sales AUTO_INCREMENT = 0")
+				if err != nil {
+					panic(err)
+				}
+				_, err = db.Exec("ALTER TABLE products AUTO_INCREMENT = 0")
+				if err != nil {
+					panic(err)
+				}
+				_, err = db.Exec("ALTER TABLE invoices AUTO_INCREMENT = 0")
+				if err != nil {
+					panic(err)
+				}
+				_, err = db.Exec("ALTER TABLE customers AUTO_INCREMENT = 0")
+				if err != nil {
+					panic(err)
+				}
+			}(db)
+
+			err = func(db *sql.DB) error {
+				_, err := db.Exec(
+					"INSERT INTO customers (`id`, `first_name`, `last_name`, `condition`) VALUES (?, ?, ?, ?)",
+					1, "John", "Doe", 1,
+				)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			}(db)
+			require.NoError(t, err)
+
+			err = func(db *sql.DB) error {
+				for _, productAttr := range testCase.products {
+					_, err := db.Exec(
+						"INSERT INTO products (`id`, `description`, `price`) VALUES (?, ?, ?)",
+						productAttr.Id, productAttr.Description, productAttr.Price,
+					)
+					if err != nil {
+						return err
+					}
+				}
+				return nil
+			}(db)
+			require.NoError(t, err)
+
+			err = func(db *sql.DB) error {
+				for _, invoiceAttr := range testCase.invoices {
+					_, err := db.Exec(
+						"INSERT INTO invoices (`customer_id`, `datetime`, `total`) VALUES (?, ?, ?)",
+						invoiceAttr.CustomerId, invoiceAttr.Datetime, invoiceAttr.Total,
+					)
+					if err != nil {
+						return err
+					}
+				}
+				return nil
+			}(db)
+			require.NoError(t, err)
+
+			err = func(db *sql.DB) error {
+				for _, saleAttr := range testCase.sales {
+					_, err := db.Exec(
+						"INSERT INTO sales (`quantity`, `product_id`, `invoice_id`) VALUES (?, ?, ?)",
+						saleAttr.Quantity, saleAttr.ProductId, saleAttr.InvoiceId,
+					)
+					if err != nil {
+						return err
+					}
+				}
+				return nil
+			}(db)
+			require.NoError(t, err)
+
+			ir := repository.NewInvoicesMySQL(db)
+			is := service.NewInvoicesDefault(ir)
+			h := handler.NewInvoicesDefault(is)
+
+			request := httptest.NewRequest("PUT", "/update_total", nil)
+			response := httptest.NewRecorder()
+
+			h.UpdateInvoicesTotal()(response, request)
+
+			require.Equal(t, testCase.expectCode, response.Code)
+
+			rows, err := db.Query("SELECT id, total FROM invoices")
+			require.NoError(t, err)
+
+			var id int
+			var total float64
+			for rows.Next() {
+				err = rows.Scan(&id, &total)
+				require.NoError(t, err)
+				total, _ = strconv.ParseFloat(fmt.Sprintf("%.2f", total), 64)
+				require.Equal(t, testCase.expectValues[id], total)
+			}
+		})
+
 	}
 }
